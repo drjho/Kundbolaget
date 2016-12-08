b48978923d8b9d8c5205c3b5d16b4b53debf322c
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-08 16:17:43 +0100

    Table structure and migration done

---
+++ Kundbolaget/Models/EntityModels/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.Models.EntityModels
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display( Name ="Orderdatum")]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [Display( Name ="Önskad leveransdatum")]
+        public DateTime DesiredDeliveryDate { get; set; }
+        
+        [Display( Name ="Planerad leveransdatum")]
+        public DateTime PlannedDeliveryDate { get; set; }    
+
+        [Required, Display(Name ="Kund id")]
+        public int  CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public virtual List<OrderProduct> OrderProducts { get; set; }
+    }
+}
