72734dcf681af5556e61c00ec5fa1ee18e69c262
Devman <Devman@DESKTOP-NN0AN6N>
2016-12-12 15:12:53 +0100

    byt ut DbStoreRepository och IStoreRepository och använd IGenericRepository för att sedan skapa DbProductRepository

---
+++ Kundbolaget/EntityFramework/Repositories/DbProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kundbolaget.EntityFramework.Context;
+using Kundbolaget.Models.EntityModels;
+
+namespace Kundbolaget.EntityFramework.Repositories
+{
+    public class DbProductRepository : IGenericRepository<Product>
+    {
+        StoreContext db = new StoreContext();    
+      
+        public Product GetItem(int productId)
+        {
+            using (var db = new StoreContext())
+            {
+                return db.Products.Include(p => p.Name).SingleOrDefault(p => p.Id == productId);
+            }
+        }
+
+        public Product[] GetItems()
+        {
+            using (var db = new StoreContext())
+            {
+                return db.Products.Include(p => p.Name).ToArray();
+            }
+        }
+
+        public void CreateItem(Product newProduct)
+        {
+            using (var db = new StoreContext())
+            {
+                db.Products.Add(newProduct);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateItem(Product updatedProduct)
+        {
+            using (var db = new StoreContext())
+            {
+                db.Products.Attach(updatedProduct);
+                var entry = db.Entry(updatedProduct);
+                entry.State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteItem(int productId)
+        {
+            using (var db = new StoreContext())
+            {
+                var product = db.Products.SingleOrDefault(p => p.Id == productId);
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- Kundbolaget/EntityFramework/Repositories/IGenericRepository.cs
+++ Kundbolaget/EntityFramework/Repositories/IGenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kundbolaget.Models.EntityModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,6 @@ namespace Kundbolaget.EntityFramework.Repositories
         T[] GetItems();
         void CreateItem(T newItem);
         void UpdateItem(T updatedItem);
-        void DeleteItem(int id);
+        void DeleteItem(int id);       
     }
 }
