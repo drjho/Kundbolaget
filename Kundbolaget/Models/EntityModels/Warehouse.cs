2a54ba89f49358c0b5844df20ccd78829eee9c0f
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-18 02:58:04 +0100

    refactor warehouse

--- Kundbolaget/Models/EntityModels/Warehouse.cs
+++ Kundbolaget/Models/EntityModels/Warehouse.cs
@@ -9,14 +9,22 @@ namespace Kundbolaget.Models.EntityModels
     public class Warehouse
     {
         public int Id { get; set; }
+
         [Display(Name = "Namn")]
         public string Name { get; set; }
+
+        [Required]
         [Display(Name = "Ort")]
         public string City { get; set; }
+
+        [Required]
         [Display(Name = "Land")]
         public string Country { get; set; }
+
+        [Required]
         [Display(Name = "Postnummer")]
         public int ZipCode { get; set; }
+
         public virtual List<StoragePlace> StoragePlace { get; set; }
 
         public Warehouse()
@@ -27,15 +35,22 @@ namespace Kundbolaget.Models.EntityModels
         void CreatePlaces(int places)
         {
             StoragePlace = new List<StoragePlace>();
-            for (int row = 0; row < 11; row++)
+            for (int aisle = 0; aisle < 11; aisle++)
             {
-                foreach( Side s in Enum.GetValues(typeof(Side)))
+                foreach (Side side in Enum.GetValues(typeof(Side)))
                 {
-                    for (int place = 0; place < 22; place++)
+                    for (int spot = 0; spot < 22; spot++)
                     {
                         for (int shelf = 0; shelf < 3; shelf++)
                         {
-                            StoragePlace.Add(new StoragePlace(row, s, place, shelf));
+                            StoragePlace.Add(new StoragePlace
+                            {
+                                AisleNr = aisle,
+                                Side = side,
+                                Spot = spot,
+                                ShelfNr = shelf
+                            });
+
                         }
                     }
                 }
