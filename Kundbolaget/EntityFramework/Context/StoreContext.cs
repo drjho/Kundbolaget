032d1e2eed6480d7cd3bb4d4ab3a0016e7ab3f7e
Devman <Devman@DESKTOP-NN0AN6N>
2016-12-19 13:06:10 +0100

    Initial Test Commit, UnitTestMoq and Class Helper

--- Kundbolaget/EntityFramework/Context/StoreContext.cs
+++ Kundbolaget/EntityFramework/Context/StoreContext.cs
@@ -15,16 +15,16 @@ namespace Kundbolaget.EntityFramework.Context
             
         }
 
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
-        public DbSet<Warehouse> Warehouses { get; set; }
-        public DbSet<StoragePlace> StoragePlaces { get; set; }
-        public DbSet <AlcoholLicense> AlcoholLicense { get; set; }
-        public DbSet<CustomerGroup> CustomerGroups { get; set; }
-        public DbSet<PriceList> PriceLists { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public virtual DbSet<Warehouse> Warehouses { get; set; }
+        public virtual DbSet<StoragePlace> StoragePlaces { get; set; }
+        public virtual DbSet<AlcoholLicense> AlcoholLicense { get; set; }
+        public virtual DbSet<CustomerGroup> CustomerGroups { get; set; }
+        public virtual DbSet<PriceList> PriceLists { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderProduct> OrderProducts { get; set; }
     }
 }
