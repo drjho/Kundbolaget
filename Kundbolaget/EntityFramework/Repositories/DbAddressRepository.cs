a576a8f3b83f5ed86ca02391cf873bae43eb5291
DevMan <DevMan@DEVMAN01>
2016-12-23 10:21:45 +0100

    Moq test för AddressController

--- Kundbolaget/EntityFramework/Repositories/DbAddressRepository.cs
+++ Kundbolaget/EntityFramework/Repositories/DbAddressRepository.cs
@@ -10,50 +10,46 @@ namespace Kundbolaget.EntityFramework.Repositories
 {
     public class DbAddressRepository : IGenericRepository<Address>
     {
+        private StoreContext db;
+
+        public DbAddressRepository()
+        {
+            db = new StoreContext();
+        }
+
+        public DbAddressRepository(StoreContext fakeContext)
+        {
+            db = fakeContext;
+        }
         public void CreateItem(Address newAddress)
         {
-            using (var db = new StoreContext())
-            {
                 db.Addresses.Add(newAddress);
                 db.SaveChanges();
-            }
         }
 
         public void DeleteItem(int addressId)
         {
-            using (var db = new StoreContext())
-            {
                 var address = db.Addresses.SingleOrDefault(c => c.Id == addressId);
                 db.Addresses.Remove(address);
                 db.SaveChanges();
-            }
         }
 
         public Address GetItem(int addressId)
         {
-            using (var db = new StoreContext())
-            {
                 return db.Addresses.SingleOrDefault(c => c.Id == addressId);
-            }
         }
 
         public Address[] GetItems()
         {
-            using (var db = new StoreContext())
-            {
                 return db.Addresses.ToArray();
-            }
         }
 
         public void UpdateItem(Address updatedAddress)
         {
-            using (var db = new StoreContext())
-            {
                 db.Addresses.Attach(updatedAddress);
                 var entry = db.Entry(updatedAddress);
                 entry.State = EntityState.Modified;
                 db.SaveChanges();
-            }
         }
     }
 }
