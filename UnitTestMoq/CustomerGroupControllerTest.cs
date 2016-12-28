e9bf4c8338dfc1d4886ce0250d86d85d73c7236d
Martin Carlsson <martingunnarcarlsson@gmail.com>
2016-12-28 09:53:05 +0100

    Tests For CustomerGroupController Done

--- UnitTestMoq/CustomerGroupControllerTest.cs
+++ UnitTestMoq/CustomerGroupControllerTest.cs
@@ -38,6 +38,68 @@ namespace UnitTestMoq
 
             _customerGroupController = new CustomerGroupController(dbCustomerGroupRepository);
         }
-    }
 
+        [Test]
+        public void Create_CustomerGroup()
+        {
+            var customergroup = new CustomerGroup
+            {
+                Id = 1,
+                Name = "Coop"          
+            };
+            _customerGroupController.Create(customergroup);
+            _mockSetCustomerGroup.Verify(x => x.Add(customergroup), Times.Once);
+            _mockContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void Delete_CustomerGroup()
+        {
+            var actionResult = _customerGroupController.Delete(1);
+            var viewResult = actionResult as ViewResult;
+            var customerGroup = (CustomerGroup) viewResult.Model;
+            Assert.AreEqual(1, customerGroup.Id);
+        }
+
+        [Test]
+        public void Edit_CustomerGroup()
+        {
+            var actionResult = _customerGroupController.Edit(1);
+            var viewResult = actionResult as ViewResult;
+            var customerGroup = (CustomerGroup)viewResult.Model;
+            Assert.AreEqual(1, customerGroup.Id);
+        }
+
+        [Test]
+        public void Details_CustomerGroup()
+        {
+            var actionResult = _customerGroupController.Details(1);
+            var viewResult = actionResult as ViewResult;
+            var customerGroup = (CustomerGroup)viewResult.Model;
+            Assert.AreEqual(1, customerGroup.Id);
+        }
+
+        [Test]
+        public void Create_Post_Redirect_To_Index()
+        {
+            var result = _customerGroupController.Create(new CustomerGroup
+            {
+                Id = 3,
+                Name = "Coop"
+            }
+                ) as RedirectToRouteResult;
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+
+        [Test]
+        public void Index_Retrieve_All_Data()
+        {
+            var actionResult = _customerGroupController.Index();
+            var viewResult = actionResult as ViewResult;
+            var viewResultModel = (CustomerGroup[])viewResult.Model;
+            var customerGroupInfo = viewResultModel.ToList();
+
+            Assert.AreEqual(1, customerGroupInfo.Count);
+        }
+    }
 }
