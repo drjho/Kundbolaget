8a2d5932f5c7f41203a267cd2c8edc1e8783d2f4
Johnson Ho <m97_j@DESKTOP-QUMH9HN>
2016-12-29 14:37:51 +0100

    DbPickingOrderRepository created.

---
+++ Kundbolaget/EntityFramework/Repositories/DbPickingOrderRepository.cs
@@ -0,0 +1,65 @@
+﻿using Kundbolaget.EntityFramework.Context;
+using Kundbolaget.Models.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.EntityFramework.Repositories
+{
+    public class DbPickingOrderRepository : IGenericRepository<PickingOrder>, IDisposable
+    {
+        private StoreContext db;
+
+        public DbPickingOrderRepository(StoreContext context)
+        {
+            db = context;
+        }
+
+        public void CreateItem(PickingOrder newItem)
+        {
+            db.PickingOrders.Add(newItem);
+            db.SaveChanges();
+        }
+
+        public void DeleteItem(int id)
+        {
+            var o = db.PickingOrders.SingleOrDefault(OrderProduct => OrderProduct.Id == id);
+            db.PickingOrders.Remove(o);
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public PickingOrder GetItem(int id)
+        {
+            return db.PickingOrders.SingleOrDefault(o => o.Id == id);
+        }
+
+        public PickingOrder[] GetItems()
+        {
+            return db.PickingOrders.ToArray();
+        }
+
+        public void UpdateItems(IEnumerable<PickingOrder> updatedItems)
+        {
+            for (int i = 0; i < updatedItems.Count(); i++)
+            {
+                db.PickingOrders.Attach(updatedItems.ElementAt(i));
+                db.Entry(updatedItems.ElementAt(i)).State = EntityState.Modified;
+            }
+            db.SaveChanges();
+        }
+
+        public void UpdateItem(PickingOrder updatedItem)
+        {
+            db.PickingOrders.Attach(updatedItem);
+            db.Entry(updatedItem).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
