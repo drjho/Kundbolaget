db3e4e7c8690b23b5664fb40909960a507ff4afa
Martin Carlsson <martingunnarcarlsson@gmail.com>
2017-01-09 13:33:24 +0100

    Added more Customers, Products, Adresses, CustomerAdresses and Alcohollicenses to Seed

--- Kundbolaget/Migrations/Configuration.cs
+++ Kundbolaget/Migrations/Configuration.cs
@@ -26,6 +26,14 @@ namespace Kundbolaget.Migrations
                 new Customer { Id = 2, Name = "ICA", CorporateStucture = "Koncern",
                     CreditLimit = -1, DaysToDelievery = 3,
                     CustomerAuditCode = 1, OrganisationNumber = "345555-7645",
+                    CustomerGroupId = 1},
+                new Customer { Id = 3, Name = "Systembolaget", CorporateStucture = "Koncern",
+                    CreditLimit = -1, DaysToDelievery = 3,
+                    CustomerAuditCode = 1, OrganisationNumber = "979999-9045",
+                    CustomerGroupId = 1},
+                new Customer { Id = 4, Name = "Lidl", CorporateStucture = "Koncern",
+                    CreditLimit = -1, DaysToDelievery = 3,
+                    CustomerAuditCode = 1, OrganisationNumber = "12245-6176",
                     CustomerGroupId = 1}
             };
 
@@ -34,7 +42,10 @@ namespace Kundbolaget.Migrations
                 new Address { Id = 1, StreetName = "Kungsgatan", Number = 1, PostalCode = "11232", Area = "Stockholm", Country = "Sweden"},
                 new Address { Id = 2, StreetName = "Kungsgatan", Number = 2, PostalCode = "11232", Area = "Stockholm", Country = "Sweden"},
                 new Address { Id = 3, StreetName = "Kungsgatan", Number = 3, PostalCode = "11232", Area = "Stockholm", Country = "Sweden"},
-                new Address { Id = 4, StreetName = "Bergsgatan", Number = 77, PostalCode = "11632", Area = "Stockholm", Country = "Sweden"}
+                new Address { Id = 4, StreetName = "Vasagatan", Number = 16, PostalCode = "11262", Area = "Stockholm", Country = "Sweden"},
+                new Address { Id = 5, StreetName = "Drottninggatan", Number = 1, PostalCode = "11292", Area = "Stockholm", Country = "Sweden"},
+                new Address { Id = 6, StreetName = "Sergelsgatan", Number = 24, PostalCode = "11272", Area = "Stockholm", Country = "Sweden"},
+                new Address { Id = 7, StreetName = "Bergsgatan", Number = 77, PostalCode = "11632", Area = "Stockholm", Country = "Sweden"}
 
             };
 
@@ -44,7 +55,22 @@ namespace Kundbolaget.Migrations
                 new Product { Id = 2, Name = "IPA", Alcohol = 4f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 33},
                 new Product { Id = 3, Name = "Strongbow", Alcohol = 4.5f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Cider, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 50},
                 new Product { Id = 4, Name = "Bishops finger", Alcohol = 5.5f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 50},
-                new Product { Id = 5, Name = "Chapel Hill", Alcohol = 12f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Mosserande, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 75}
+                new Product { Id = 5, Name = "Chapel Hill", Alcohol = 12f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Mosserande, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 75},
+                new Product { Id = 6, Name = "Absolut", Alcohol = 40f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Starksprit, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 100},
+                new Product { Id = 7, Name = "Mintuu", Alcohol = 40f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Starksprit, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 50},
+                new Product { Id = 8, Name = "Fireball", Alcohol = 38f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Starksprit, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 50},
+                new Product { Id = 9, Name = "Carlsberg", Alcohol = 5.2f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Burk, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Flak, VatCode = 1, Volume = 50},
+                new Product { Id = 10, Name = "Mariestad", Alcohol = 5.3f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Burk, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Flak, VatCode = 1, Volume = 50},
+                new Product { Id = 11, Name = "Stockholms", Alcohol = 5.2f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Burk, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Flak, VatCode = 1, Volume = 50},
+                new Product { Id = 12, Name = "Tre Apor", Alcohol = 10.3f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Box, ConsumerPerStorage = 8, ProductGroup = ProductGroup.Vitvin, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 300},
+                new Product { Id = 13, Name = "Masi Campofiorin", Alcohol = 11.3f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Box, ConsumerPerStorage = 8, ProductGroup = ProductGroup.Rödvin, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 300},
+                new Product { Id = 14, Name = "Staropramen", Alcohol = 5.2f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 33},
+                new Product { Id = 15, Name = "Groolsh", Alcohol = 5.4f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Öl, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 33},
+                new Product { Id = 16, Name = "Summersby", Alcohol = 4.5f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Cider, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 33},
+                new Product { Id = 17, Name = "Rekordelig", Alcohol = 7.0f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Burk, ConsumerPerStorage = 24, ProductGroup = ProductGroup.Cider, StoragePackage = StoragePackage.Flak, VatCode = 1, Volume = 50},
+                new Product { Id = 18, Name = "Don Pergnon", Alcohol = 12f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Mosserande, StoragePackage = StoragePackage.Back, VatCode = 1, Volume = 75},
+                new Product { Id = 19, Name = "Jack Daniels", Alcohol = 38f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Starksprit, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 100},
+                new Product { Id = 20, Name = "Highland Park", Alcohol = 40f, AuditCode = 1, ConsumerPackage = ConsumerPackage.Flaska, ConsumerPerStorage = 12, ProductGroup = ProductGroup.Starksprit, StoragePackage = StoragePackage.Kartong, VatCode = 1, Volume = 75}
             };
 
             var warehouses = new Warehouse[]
@@ -110,7 +136,10 @@ namespace Kundbolaget.Migrations
                 new CustomerAddress { Id = 4, CustomerId =1, AddressId = 3, AddressType = AddressType.Besök },
                 new CustomerAddress { Id = 5, CustomerId =2, AddressId = 4, AddressType = AddressType.Leverans },
                 new CustomerAddress { Id = 6, CustomerId =2, AddressId = 4, AddressType = AddressType.Faktura },
-
+                new CustomerAddress { Id = 5, CustomerId =3, AddressId = 6, AddressType = AddressType.Leverans },
+                new CustomerAddress { Id = 6, CustomerId =3, AddressId = 6, AddressType = AddressType.Faktura },
+                new CustomerAddress { Id = 5, CustomerId =4, AddressId = 7, AddressType = AddressType.Leverans },
+                new CustomerAddress { Id = 6, CustomerId =4, AddressId = 7, AddressType = AddressType.Faktura }
 
             };
 
@@ -128,6 +157,8 @@ namespace Kundbolaget.Migrations
                 new AlcoholLicense {Id = 1, CustomerId = 1, StartDate = DateTime.Parse("2016-01-01"), EndDate = DateTime.Parse("2016-12-31") },
                 new AlcoholLicense {Id = 2, CustomerId = 1, StartDate = DateTime.Parse("2017-01-01"), EndDate = DateTime.Parse("2017-12-31") },
                 new AlcoholLicense {Id = 3, CustomerId = 2, StartDate = DateTime.Parse("2017-01-01"), EndDate = DateTime.Parse("2017-12-31") },
+                new AlcoholLicense {Id = 4, CustomerId = 3, StartDate = DateTime.Parse("2017-01-01"), EndDate = DateTime.Parse("2017-12-31") },
+                new AlcoholLicense {Id = 5, CustomerId = 4, StartDate = DateTime.Parse("2017-01-01"), EndDate = DateTime.Parse("2017-12-31") },
             };
 
             context.Warehouses.AddOrUpdate(warehouses);
