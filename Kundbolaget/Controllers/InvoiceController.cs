79e3eee5a6eb36452c328919e1eb195b02235461
pontu <pontu@DESKTOP-TGD38VF>
2017-01-11 17:07:54 +0100

    Invoice_model,repo_created

---
+++ Kundbolaget/Controllers/InvoiceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kundbolaget.EntityFramework.Context;
+using Kundbolaget.EntityFramework.Repositories;
+
+namespace Kundbolaget.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private DbInvoiceDepository invoiceRepo;
+        private DbOrderRepository orderRepo;
+        private DbOrderProductRepository orderProductRepo;
+        private DbCustomerRepository customerRepo;
+        private DbProductRepository productRepository;
+        private DbPriceListRepository priceListRepository;
+
+
+        public InvoiceController()
+        {
+            StoreContext db = new StoreContext();
+            invoiceRepo = new DbInvoiceDepository(db);
+            orderRepo = new DbOrderRepository(db);
+
+            
+        }
+        // GET: Invoice
+        public ActionResult Index(DateTime? searchDate)
+        {
+            var model = invoiceRepo.GetItems();
+            if (searchDate.HasValue)
+            {
+                model = model.Where(x => x.PayBefore == searchDate).ToArray();
+            }
+            return View(model);
+        }
+        //GET: Invoice/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var invoice = invoiceRepo.GetItem((int) id);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            return View();
+        }
+    }
+}
---
+++ Kundbolaget/Models/EntityModels/Invoice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kundbolaget.Models.EntityModels
+{
+    public class Invoice
+    {
+        [Display(Name = "Order id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Faktureringsdatum")]
+        public DateTime InvoiceDate { get; set; }
+
+        [Display(Name = "Produkt")]
+        public virtual List<Product> Product { get; set; }
+
+        [Display(Name = "Betald")]
+        public bool Paid { get; set; }
+
+        [Display(Name = "Förfallodag")]
+        public DateTime PayBefore {
+            get { return this.InvoiceDate.Date.AddDays(15); }
+            set { this.InvoiceDate.Date.AddDays(15); } }
+
+        [Display(Name = "Kund")]
+        public virtual Customer Customer { get; set; }
+
+        [Display(Name = "Kundadress")]
+        public Address Address { get; set; }
+
+        
+
+        //public PriceList PriceList { get; set; }
+
+        //[Required]
+        //[Display(Name = "Rabatt")]
+        //public int Rebate { get; set; }
+
+        //[Display(Name = "Antal")]
+        //public int ProductAmount { get; set; }
+        //[Display(Name = "Momskod")]
+        //public decimal VatCode { get; set; }
+
+        //[Required]
+        //[Display(Name = "Pris")]
+        //public decimal Price { get; set; }
+    }
+}
